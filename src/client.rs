f2a6718b5dca19764f2b8f3294aa84acb0c29922
BadAimWeeb <badaimweeb@protonmail.com>
2024-12-07 09:19:25 -0600

    uh

--- src/client.rs
+++ src/client.rs
@@ -1,99 +1,113 @@
-use futures_util::{stream::FusedStream, FutureExt, Sink, SinkExt, Stream, StreamExt};
+use futures_util::{stream::FusedStream, FutureExt, Sink, SinkExt, Stream, StreamExt, task::Poll::{Ready, Pending}};
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet, LinkedList},
     pin::Pin,
     task::{Context, Poll},
 };
 
 pub struct DTSocketClient {
-    protov2d: Box<rs_protov2d::client::Client>,
+    protov2d: rs_protov2d::client::Client,
 
     nonce_counter: u64,
-    t0_callbacks: HashMap<u64, tokio::sync::oneshot::Sender<(bool, Vec<u8>)>>,
+
+    t0_register: HashSet<u64>,
+    backfeed: LinkedList<DTPacketType>
 }
 
 pub enum DTPacketType {
-
+    Type0 {
+        nonce: u64,
+        success: bool,
+        data: Vec<u8>
+    }
 }
 
 pub struct DTPacket {
     pub packet_type: DTPacketType,
-    pub data: Vec<u8>,
+    pub data: Vec<u8>
 }
 
 impl Stream for DTSocketClient {
-    type Item = ();
+    type Item = DTPacketType;
 
     fn poll_next(
         mut self: Pin<&mut DTSocketClient>,
         cx: &mut Context<'_>,
     ) -> Poll<Option<Self::Item>> {
         loop {
-            match futures_util::ready!(self.protov2d.next().poll_unpin(cx)) {
-                Some(msg) => {
-                    if msg.qos != 1 {
-                        continue;
-                    }
-
-                    let data = msg.data;
-                    let t = data[0];
-                    let mut data = &data[1..];
+            match (self.protov2d.next().poll_unpin(cx)) {
+                Ready(t) => {
                     match t {
-                        0 => {
-                            // handle packet type 0
-                            let d = rmpv::decode::read_value(&mut data);
-                            if d.is_err() {
+                        Some(msg) => {
+                            if msg.qos != 1 {
                                 continue;
                             }
-
-                            let d = d.unwrap();
-                            let d = d.as_array();
-                            if d.is_none() {
-                                continue;
+        
+                            let data = msg.data;
+                            let t = data[0];
+                            let mut data = &data[1..];
+                            match t {
+                                0 => {
+                                    // handle packet type 0
+                                    let d = rmpv::decode::read_value(&mut data);
+                                    if d.is_err() {
+                                        continue;
+                                    }
+        
+                                    let d = d.unwrap();
+                                    let d = d.as_array();
+                                    if d.is_none() {
+                                        continue;
+                                    }
+                                    let d = d.unwrap();
+                                    if d.len() != 3 {
+                                        continue;
+                                    }
+        
+                                    let nonce = d[0].as_u64();
+                                    if nonce.is_none() {
+                                        continue;
+                                    }
+                                    let nonce = nonce.unwrap();
+                                    
+                                    if !self.t0_register.contains(&nonce) {
+                                        continue;
+                                    }
+                                    self.t0_register.remove(&nonce);
+        
+                                    let success = d[1].as_bool();
+                                    if success.is_none() {
+                                        continue;
+                                    }
+                                    let success = success.unwrap();
+        
+                                    let mut v: Vec<u8> = Vec::new();
+                                    let result = rmpv::encode::write_value(&mut v, &d[2]);
+                                    if result.is_err() {
+                                        continue;
+                                    }
+        
+                                    return Poll::Ready(Some(DTPacketType::Type0 { nonce, success, data: v }));
+                                }
+        
+                                _ => {
+                                    continue;
+                                }
                             }
-                            let d = d.unwrap();
-                            if d.len() != 3 {
-                                continue;
-                            }
-
-                            let nonce = d[0].as_u64();
-                            if nonce.is_none() {
-                                continue;
-                            }
-                            let nonce = nonce.unwrap();
-                            
-                            if !self.t0_callbacks.contains_key(&nonce) {
-                                continue;
-                            }
-
-                            let success = d[1].as_bool();
-                            if success.is_none() {
-                                continue;
-                            }
-                            let success = success.unwrap();
-
-                            let mut v: Vec<u8> = Vec::new();
-                            let result = rmpv::encode::write_value(&mut v, &d[2]);
-                            if result.is_err() {
-                                continue;
-                            }
-
-                            let result = self.t0_callbacks.remove(&nonce);
-                            if result.is_none() {
-                                continue;
-                            }
-
-                            let result = result.unwrap();
-                            let _ = result.send((success, v));
                         }
-
-                        _ => {
-                            continue;
+                        
+                        None => {
+                            return Poll::Ready(None);
                         }
                     }
                 }
-                None => {
-                    return Poll::Ready(None);
+                
+                Pending => {
+                    if !self.backfeed.is_empty() {
+                        return Poll::Ready(Some(self.backfeed.pop_front().unwrap()));
+                    }
+
+                    return Poll::Pending;
                 }
             }
         }
@@ -101,11 +115,12 @@ impl Stream for DTSocketClient {
 }
 
 impl DTSocketClient {
-    pub fn new(protov2d: Box<rs_protov2d::client::Client>) -> Self {
+    pub fn new(protov2d: rs_protov2d::client::Client) -> Self {
         Self {
             protov2d,
             nonce_counter: 0,
-            t0_callbacks: HashMap::new(),
+            t0_register: HashSet::new(),
+            backfeed: LinkedList::new()
         }
     }
 
@@ -124,20 +139,35 @@ impl DTSocketClient {
 
         let serialized = serialized.unwrap();
 
-        let _ = self.protov2d.send(1, serialized);
+        let _ = self.protov2d.send_packet(1, serialized).await;
 
-        let (tx, rx) = tokio::sync::oneshot::channel();
-        self.t0_callbacks.insert(nonce, tx);
+        self.t0_register.insert(nonce);
+        let r_packet;
+        loop {
+            let packet = self.next().await;
+            if packet.is_some() {
+                let up = packet.unwrap();
+                match up {
+                    DTPacketType::Type0 { nonce: p_nonce, success, data } => {
+                        if nonce != p_nonce {
+                            self.backfeed.push_back(DTPacketType::Type0 { nonce: p_nonce, success, data });
+                            continue;
+                        }
 
-        let result = rx.await;
-        self.t0_callbacks.remove(&nonce);
-        if result.is_err() {
-            return Err("internal_client_error: failed to receive data".to_string());
+                        r_packet = (success, data);
+                        break;
+                    }
+                    _ => {
+                        self.backfeed.push_back(up);
+                    }
+                }
+            }
+
+            panic!("failure");
         }
 
-        let result = result.unwrap();
-        if !result.0 {
-            let err: Result<String, _> = rmp_serde::from_slice(&result.1);
+        if !r_packet.0 {
+            let err: Result<String, _> = rmp_serde::from_slice(&r_packet.1);
             if err.is_err() {
                 return Err("internal_client_error: failed to deserialize".to_string());
             }
@@ -145,7 +175,7 @@ impl DTSocketClient {
             return Err(err.unwrap());
         }
 
-        let result: Result<O, _> = rmp_serde::from_slice(&result.1);
+        let result: Result<O, _> = rmp_serde::from_slice(&r_packet.1);
         if result.is_err() {
             return Err("internal_client_error: failed to deserialize".to_string());
         }
--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,66 @@
+use std::{process::exit, sync::Arc};
+
+use client::DTSocketClient;
+use futures_util::{StreamExt as _};
+use rs_protov2d::client::{ClientHandshakeConfig, PublicKey, PublicKeyType};
+use async_mutex::Mutex;
+
 use crate::*;
+
+#[derive(serde::Deserialize)]
+struct VersionDeserialize {
+    pub version: String,
+    pub branch: String,
+    pub commit: String
+}
 
 #[tokio::test]
 async fn connect() {
-    
+    rustls::crypto::aws_lc_rs::default_provider().install_default().expect("Failed to install rustls crypto provider");
+
+    let result = rs_protov2d::client::Client::connect("wss://id-backend.badaimweeb.me/", ClientHandshakeConfig {
+        public_keys: vec![PublicKey {
+            key_type: PublicKeyType::Hash,
+            data:Some("357093b00d1d5640aae631dd62519a88b29233274df4a23fd960432d8004ecb9".to_string())
+        }],
+        ping_interval: None
+    }).await;
+
+    if result.is_err() {
+        println!("{:?}", result.err().unwrap());
+        panic!("connection should be successful");
+    }
+
+    let client = result.unwrap();
+    println!("connected to BAW#ID backend");
+
+    let dt = DTSocketClient::new(client);
+    let rc = Arc::new(Mutex::new(dt));
+    let rc2 = rc.clone();
+
+    let t = tokio::spawn(async move {
+        let mut dt = rc.lock().await;
+        println!("locked lol");
+        let response = dt.call_procedure("serverVersion", 0);
+        let response: Result<VersionDeserialize, _> = response.await;
+
+        if response.is_err() {
+            println!("{:?}", response.err().unwrap());
+            panic!("serverVersion should be successful");
+        }
+
+        let response = response.unwrap();
+        println!("BAW#ID server version is {} ({}@{})", response.version, response.branch, response.commit);
+    });
+
+    tokio::spawn(async move {
+        loop {
+            let mut dt = rc2.lock().await;
+            let _ = dt.next().await;
+        }
+    });
+
+    t.await.unwrap();
+    exit(0);
 }
 
